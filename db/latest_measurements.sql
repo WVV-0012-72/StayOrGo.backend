f0faf3c9785b2eca8b3bb5a17e06f3bef01d058f
Michael Lenz <michael.lenz@nullvector.org>
2020-03-21 22:24:52 +0100

    get latest meas: cleanup

--- db/latest_measurements.sql
+++ db/latest_measurements.sql
@@ -1,6 +1,6 @@
 -- get latest number of clients and thresholds for each acq fe
-SELECT m.fe_name, m.m_num_devices, c.c_threshold_y, c.c_threshold_r
+SELECT m.m_time, m.fe_name, m.m_num_devices, c.c_threshold_y, c.c_threshold_r
 FROM measurement m
 INNER JOIN coverage c ON c.fe_name=m.fe_name
 WHERE m.m_time=(SELECT max(t.m_time) FROM measurement t WHERE t.fe_name=m.fe_name)
-ORDER BY m.m_time DESC;
+ORDER BY m.m_time, m.fe_name;
