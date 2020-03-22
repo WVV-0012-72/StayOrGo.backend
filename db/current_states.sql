126a11ffe5ba135beea7022d07c5b6e1fe3906cb
Michael Lenz <michael.lenz@nullvector.org>
2020-03-22 11:05:15 +0100

    get_current_states: Should I stay or should I go?
    quick SQL hack to extract Stay!/Try../Go! from the current number of
    people and the thresholds defined for the respective ACQ FE.

---
+++ db/current_states.sql
@@ -0,0 +1,15 @@
+-- get latest number of clients and thresholds for each acq fe
+SELECT m.m_time, m.fe_name,
+	c.c_position, c.c_radius,
+	m.m_num_devices,
+	CASE m.m_num_devices
+		WHEN m.m_num_devices < c.c_threshold_y THEN "Go!"
+		WHEN m.m_num_devices < c.c_threshold_r THEN "Try.."
+		ELSE "Stay!"
+	END
+FROM measurement m
+INNER JOIN coverage c ON c.fe_name=m.fe_name
+WHERE m.m_time=(SELECT max(t.m_time)
+				FROM measurement t
+				WHERE t.fe_name=m.fe_name)
+ORDER BY m.m_time, m.fe_name;
