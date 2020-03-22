caca3e151bab402a6f04f58284f0f546fdac5124
Michael Lenz <michael.lenz@nullvector.org>
2020-03-22 11:17:26 +0100

    ottmar.wifi: try not to crash
    making this code a wee bit less of a hack, so that it won't keep
    crashing all the time with DB issues, dweet.io issues or the like

--- ottmar.wifi.py
+++ ottmar.wifi.py
@@ -12,7 +12,13 @@ db = sqlite3.connect('stay_or_go.db')
 
 
 while True:
-    dweets = dweepy.get_latest_dweet_for(pibox)
+    try:
+        dweets = dweepy.get_latest_dweet_for(pibox)
+    except:
+        print("dweet.io oops..")
+        time.sleep(1)
+        continue
+
     for dweet in dweets:
         # get dweet data
         d_thing = str(dweet['thing'])
@@ -39,16 +45,22 @@ while True:
 
         # update frontend status
         # TODO: check that a frontend with fe_name exists, create in doubt
-        fe_update= ("UPDATE frontend SET "
-            "   fe_last_time='"+d_timestamp+"', "
-            "   fe_cputemp='"+d_cputemp+"' "
-            "WHERE fe_name='"+d_thing+"';")
-        db.execute(fe_update);
+        try:
+            fe_update= ("UPDATE frontend SET "
+                "   fe_last_time='"+d_timestamp+"', "
+                "   fe_cputemp='"+d_cputemp+"' "
+                "WHERE fe_name='"+d_thing+"';")
+            db.execute(fe_update);
+        except:
+            print("fe status update failed")
 
 
         # write measurement into database
-        db.execute("INSERT INTO measurement VALUES(?, ?, ?, ?)", measurement)
-        db.commit()
+        try:
+            db.execute("INSERT INTO measurement VALUES(?, ?, ?, ?)", measurement)
+            db.commit()
+        except:
+            print("measurement update failed")
 
         # hush little baby, don't say a word.. :-)
         time.sleep(1)
